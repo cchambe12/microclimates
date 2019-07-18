0bd0fb0c306b9b32fbb81eb74293a081ad2d7c33
cchambe12 <cchamberlain@g.harvard.edu>
2019-07-18 12:26:36 -0400

    Update Get.micro.phylo.R

--- phylodata/Get.micro.phylo.R
+++ phylodata/Get.micro.phylo.R
@@ -103,9 +103,50 @@ newnames <- data.frame(tiplabel=phy.plants.micro$tip.label)
 newnames$genus <- gsub("_.*", "", newnames$tiplabel)
 newnames$species <- gsub(".*_", "", newnames$tiplabel)
 newnames$labels <- paste(newnames$genus, newnames$species)
-newnames$labels <- italic(newnames$labels)
 
-ggtree(phy.plants.micro)  + geom_tiplab(size=3, label=newnames$labels, fontface='italic') + 
-  theme(plot.margin = unit(c(0,1,1,1), "lines")) +
-  xlim(c(0, 200))
+tt <- as.data.frame(table(gdd.stan$site, gdd.stan$latbi))
+names(tt) <- c("site", "phylo", "freq")
+tt$freq <- ifelse(tt$freq==0, NA, tt$freq)
 
+tt <- tt[!is.na(tt$freq),]
+tt$freq <- NULL
+
+arb <- tt[(tt$site=="arb"),]
+arbids <- unique(arb$phylo)
+
+hf <- tt[(tt$site=="hf"),]
+hfids <- unique(hf$phylo)
+
+cg <- tt[(tt$site=="cg"),]
+cgids <- unique(cg$phylo)
+
+tiplabels <- data.frame(phylo=unique(tt$phylo))
+tiplabels$arb <- ifelse(tiplabels$phylo%in%arbids, 1, 0)
+tiplabels$hf <- ifelse(tiplabels$phylo%in%hfids, 1, 0)
+tiplabels$cg <- ifelse(tiplabels$phylo%in%cgids, 1, 0)
+tiplabels <- tiplabels[(tiplabels$phylo!="NA_NA"),]
+
+arb.df<-select_(tiplabels, "arb")
+arb.df$col <- ifelse(arb.df$arb==0, "white", "red")
+cg.df<-select_(tiplabels, "cg")
+cg.df$col <- ifelse(cg.df$cg==0, "white", "darkgreen")
+hf.df<-select_(tiplabels, "hf")
+hf.df$col <- ifelse(hf.df$hf==0, "white", "blue4")
+
+
+##plot.margin: (t, r, b, l) *think 'trouble'
+tree.plot <- ggtree(phy.plants.micro)  + geom_tiplab(size=3, label=newnames$labels, fontface='italic') + 
+  theme(plot.margin = unit(c(1,3,1,1), "lines")) + geom_tippoint(col=arb.df$col) + geom_tippoint(col=cg.df$col) +
+  geom_tippoint(col=hf.df$col) +
+  xlim(c(0, 150))
+
+tree.plot <- tree.plot %<+% tt + geom_tippoint(aes(group=c(tt$phylo, tt$site)))
+#tree.plot+theme(legend.position="right")
+
+quartz()
+tree.plot
+
+
+arb.df<-arb.df$arb
+colors<-setNames(c("white","red"),c(0,1))
+dotTree(phy.plants.micro,arb.df,colors=colors,data.type="discrete",fsize=0.7,x.space=0.05)
