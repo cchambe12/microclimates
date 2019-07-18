09e05404a6fbbe571428f825685d3d381b327d27
cchambe12 <cchamberlain@g.harvard.edu>
2019-07-18 18:42:41 -0400

    more models

---
+++ analyses/exp_muplot_brms.R
@@ -0,0 +1,42 @@
+## Started 3 April 2019 ##
+## By Cat - based off code from Lizzie's OSPREE plots ##
+
+# Runs from models_stan_plotting.R #
+
+# with COLORS for each species #
+
+muplotfx <- function(modelhere, nameforfig, width, height, ylim, xlim, leg1, leg2){
+  spnum <- unique(gdd$spp)
+  pdf(file.path(figpath, paste("", nameforfig, figpathmore, ".pdf", sep="")),
+      width = width, height = height)
+  par(xpd=FALSE)
+  par(mar=c(5,7,3,10))
+  plot(x=NULL,y=NULL, xlim=xlim, yaxt='n', ylim=ylim,
+       xlab=xlab, ylab="", main=nameforfig)
+  axis(2, at=1:1, labels=rev(c("Provenance \nLatitude")),las=1)
+  abline(v=0, lty=2, col="darkgrey")
+  rownameshere <- c("provenance.lat")
+  ppeffects <- c("provenance.lat") # or 1:4 here...
+  for(i in 1:1){#i=1
+    pos.y<-(1:1)[i]
+    pos.x<-modoutput[(modoutput$term==rownameshere[i]),"estimate"]
+    lines(modoutput[(modoutput$term==rownameshere[i]),c("lower","upper")],rep(pos.y,2),col="darkgrey")
+    points(pos.x,pos.y,cex=1.5,pch=19,col="darkblue")
+    for(spsi in 1:length(spnum)){#spsi=1
+      pos.sps.i<-which(grepl(paste("[",spsi,"]",sep=""),mod.ranef$parameter,fixed=TRUE))
+      jitt<-runif(1,0.05,0.4)
+      pos.y.sps.i<-pos.y-jitt
+      pos.x.sps.i<-mod.ranef[pos.sps.i[i],"mean"]
+      lines(mod.ranef[pos.sps.i[i],c("lower","upper")],rep(pos.y.sps.i,2),
+            col=alpha(my.pal[spsi], alphahere))
+      points(pos.x.sps.i,pos.y.sps.i,cex=0.8, col=alpha(my.pal[spsi], alphahere))
+      
+    }
+  }
+  par(xpd=TRUE) # so I can plot legend outside
+  legend(leg1, leg2, sort(unique(gsub("_", " ", gdd$spp))), pch=my.pch[1:length(spnum)],
+         col=alpha(my.pal[1:length(spnum)], alphahere),
+         cex=1, bty="n", text.font=3)
+  dev.off()
+  
+}
--- analyses/models_stan_brms.R
+++ analyses/models_stan_brms.R
@@ -92,29 +92,31 @@ mod.tsdvr <- brm(gdd_dvr ~ spp, data=ts.gdd, control=list(max_treedepth = 15,ada
 
 #### Let's make side by side plots now...
 library(broom)
-sitemod<-as.data.frame(tidy(mod.gddbb, prob=0.5))
-modoutput <- sitemod #modelhere
+station.mod<-as.data.frame(tidy(mod_station, prob=0.5))
+modoutput <- station.mod #modelhere
 
-modoutput<-modoutput[2:4,]
+modoutput<-modoutput[1:15,]
 modoutput$`25%` <- modoutput$lower
 modoutput$`75%` <- modoutput$upper
 modoutput$term<-gsub(".*b_","",modoutput$term)
 modoutput$term<-gsub(".*spp","",modoutput$term)
-modoutput$term <- ifelse(modoutput$term=="provenance.lat", "xlat", modoutput$term)
-modoutput$Jvar <- as.numeric(rev(as.factor(modoutput$term)))
+#modoutput$term <- ifelse(modoutput$term=="provenance.lat", "xlat", modoutput$term)
+modoutput$Jvar <- rev(as.numeric(as.factor(modoutput$term)))
 
-estimates<-c("Tree Spotters","Common Garden", "Harvard Forest")
+#estimates<-c("Tree Spotters","Common Garden", "Harvard Forest")
 
 #estimates<-c("Acer saccharum", "Aesculus flava", "Betula alleghaniense", "Betula nigra", "Carya glabra", "Carya ovata",
  #            "Fagus grandifolia", "Hamamelis virginiana", "Populus deltoides", "Quercus alba", "Quercus rubra", "Tilia americana",
   #           "Vaccinium corymbosum", "Viburnum nudum", "Provenance Latitude")
+estimates<-c("Intercept", sort(unique(gdd.hobo$spp[(gdd.hobo$spp!="Acepen")])))
+
 estimates<-rev(estimates)
 modoutput <- modoutput[!is.na(modoutput$Jvar),]
 muplot<-ggplot(modoutput, aes(x=`25%`, xend=`75%`, y=Jvar, yend=Jvar)) +
   geom_vline(xintercept=0, linetype="dotted") + geom_point(aes(x=estimate, y=Jvar), col="black") +
   geom_segment(arrow = arrow(length = unit(0.00, "npc"))) +
   guides(size=FALSE) +
-  scale_y_discrete(limits = sort(unique(modoutput$term)), labels=estimates) +
+  scale_y_discrete(limits = rev(unique(modoutput$term))) +
   xlab("Change in growing degree days \nfrom budburst to leafout") + ylab("") + theme_linedraw() +
   theme(legend.text=element_text(size=5), legend.title = element_text(size=9), legend.background = element_rect(linetype="solid", color="grey", size=0.5),
         panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
@@ -122,7 +124,7 @@ muplot<-ggplot(modoutput, aes(x=`25%`, xend=`75%`, y=Jvar, yend=Jvar)) +
         text=element_text(family="sans"), legend.position = "none",
         legend.text.align = 0,
         plot.margin = unit(c(1,1,1,1), "lines")) +  #+ ggtitle("Original Parameters") +
-  coord_cartesian(xlim=c(-150, 150), ylim=c(1,3), clip = 'off') #+ ggtitle("Hobo Loggers") 
+  coord_cartesian(xlim=c(-150, 500), ylim=c(1,15), clip = 'off') #+ ggtitle("Hobo Loggers") 
 
 station<-as.data.frame(tidy(mod.tsdvr, prob=0.5))
 modoutput <- station #modelhere
