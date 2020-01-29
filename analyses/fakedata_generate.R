c178ed1361013066d218ccb5500b7839216049b2
cchambe12 <cchamberlain@g.harvard.edu>
2020-01-29 18:52:10 -0500

    updating fake data

--- analyses/fakedata_generate.R
+++ analyses/fakedata_generate.R
@@ -84,22 +84,265 @@ data1 <- data.frame(
   sim = alpha[fakedata_ws$species] +
     rnorm(nsims, mean = 0, sd = sigma)
 )
-## sample_a <- rnorm(n = 10000, mean = mean(d2$height), sd = 100) # assume intercept is centered on mean height
-## sample_b <- rnorm(n = 10000, mean = 0, sd = 10) # slope centered on 0
-## sample_sigma <- runif(n = 10000, min = 0, max = 50)
-## sample_weight <- sample(x = seq(4, 48, by = 1), size = 10000, replace = TRUE) # generate some random weights
-## sample_mu <- sample_a + (sample_b * sample_weight) # generate mu using linear model and weights
-## sample_height <- rnorm(n = 10000, mean = sample_mu, sd = sample_sigma)
-## plot(density(sample_height))
-## abline(v = mean(d2$height), lwd = 2, col = "blue")
+
+xysim_labs <- labs(
+  x = "Observed GDD",
+  y = "Simulated data"
+)
+
+theme_set(bayesplot::theme_default(base_size = 18))
+theme_update(axis.text = element_text(size = 20))
+
+ggplot(data1, aes(x = gdd, y = sim)) + 
+  geom_point(alpha = 0.1, color = "red") + 
+  xysim_labs + coord_cartesian(xlim=c(0, 1500), ylim=c(0,1500)) + geom_abline(intercept=0, slope=1)
+
+## Using weakly informative priors
+alpha2 <- rnorm(20, 0, 1)
+sigma2 <- runif(20, 0, 1)
+
+data2 <- data.frame(
+  gdd = fakedata_ws$gdd,
+  sim2 = alpha2[fakedata_ws$species] +
+    rnorm(nsims, mean = 0, sd = sigma)
+)
+
+xysim_labs <- labs(
+  x = "Observed GDD",
+  y = "Simulated data"
+)
+
+theme_set(bayesplot::theme_default(base_size = 18))
+theme_update(axis.text = element_text(size = 20))
+
+ggplot(data2, aes(x = gdd, y = sim2)) + 
+  geom_point(alpha = 0.1, color = "red") + 
+  xysim_labs + coord_cartesian(xlim=c(0, 1500), ylim=c(0,1500)) + geom_abline(intercept=0, slope=1)
+
+data3 <- data.frame(
+  gdd = fakedata_ws$gdd, 
+  wip = data2$sim, 
+  vague = data1$sim
+)
+ggplot(data3, aes(x=gdd, y=wip)) + 
+  geom_point(alpha = 0.1) + 
+  geom_point(
+    aes(y = vague), 
+    color = "red", 
+    alpha = 0.1
+  ) + 
+  xysim_labs + coord_cartesian(xlim=c(0, 1500), ylim=c(0,1500)) + geom_abline(intercept=0, slope=1)
 
 
-fakedata_ws$LL <- sapply( 1:nrow(fakedata_ws) , function(i) sum( dnorm(
-  fakedata_ws$gdd ,
-  mean=mean(fakedata_ws$gdd),
-  sd=sd(fakedata_ws$gdd),
-  log=FALSE ) ) )
-fakedata_ws$prod <- fakedata_ws$LL + dnorm( mean(fakedata_ws$gdd) , 700 , 200 , FALSE ) +
-  dunif( sd(fakedata_ws$gdd) , 0 , 200 , FALSE )
-fakedata_ws$prob <- exp( fakedata_ws$prod - max(fakedata_ws$prod) )
+##### NOW FOR HOBO LOGGER FAKE DATA!
+### Okay, now let's make some fake data using help Rethinking, Gelman, OSPREE and Geoff
+#  1) Let's make the observations much higher than the actual data to build a good model.
+nsp = 20 # number of species
+ntot = 50 # numbers of obs per species. 
+
+sample_a <- list(int.env = rnorm(1000, 600, 50))
+
+#  2) Now, we will make varying intercepts
+int.samples <- sapply(sample_a, FUN = function(x){
+  sample(x, size = nsp * ntot, replace = TRUE)})
+
+#  4) We need to make a random intercept model for each species
+baseinter <- list(intercept = mean(int.samples))
+baseinter.mat <- matrix(unlist(baseinter), ncol = length(baseinter), nrow = nsp * ntot, byrow = TRUE)
+## From Geoff's simulate-linear.R code in OSPREE: Which parameters are random?
+random.inter <- grep(pattern = paste("intercept", collapse = "|"), x = names(baseinter))
+# Generate random intercepts (by species) 
+for(i in 1:length(random.inter)){
+  baseinter.mat[, i] <- sapply(1:nsp, FUN = function(X){
+    rep(rnorm(n = 1, mean = baseinter[[random.inter[i]]], sd = 50), ntot)})}
+
+#  5) Calculate response
+response <- sapply(1:nrow(int.samples), FUN = function(x){
+  rnorm(n = 1, mean = baseinter.mat[x, ], sd = 50)})
+#  6) Make a dataframe of fake data
+fakedata_ws <- cbind(data.frame(species = as.vector(sapply(1:nsp, FUN = function(x) rep(x, ntot))),
+                                gdd = response))
+
+#  7) Let's do a quick lmer model to test the fake data
+library(lme4)
+modtest <- lmer(gdd ~ 1 + (1|species), data=fakedata_ws) ## Quick look looks good!
+
+library(RColorBrewer)
+my.pal <- rep(brewer.pal(n = 10, name = "Paired"), 2)
+my.pch <- rep(15:16, each=10)
+plot(gdd ~ species, col=my.pal[species], pch=my.pch[species], data = fakedata_ws)
+abline(h=mean(fakedata_ws$gdd), lwd=3)
+
+plot(density(fakedata_ws$gdd))
+abline(v = mean(fakedata_ws$gdd), lwd = 2, col = "blue")
+
+## PRIOR PREDICTIVE CHECK time!!
+# Now I will follow the workflow from the Gabry et al., 2019 paper
+## Using vague priors
+nsims <- length(fakedata_ws$species)
+alpha <- rnorm(20, 600, 50)
+sigma <- runif(20, 0, 50)
+
+data1 <- data.frame(
+  gdd = fakedata_ws$gdd,
+  sim = alpha[fakedata_ws$species] +
+    rnorm(nsims, mean = 0, sd = sigma)
+)
+
+xysim_labs <- labs(
+  x = "Observed GDD",
+  y = "Simulated data"
+)
+
+theme_set(bayesplot::theme_default(base_size = 18))
+theme_update(axis.text = element_text(size = 20))
+
+ggplot(data1, aes(x = gdd, y = sim)) + 
+  geom_point(alpha = 0.1, color = "red") + 
+  xysim_labs + coord_cartesian(xlim=c(250, 1000), ylim=c(250,1000)) + geom_abline(intercept=0, slope=1)
+
+## Using weakly informative priors
+alpha2 <- rnorm(20, 0, 1)
+sigma2 <- runif(20, 0, 1)
+
+data2 <- data.frame(
+  gdd = fakedata_ws$gdd,
+  sim2 = alpha2[fakedata_ws$species] +
+    rnorm(nsims, mean = 0, sd = sigma)
+)
+
+xysim_labs <- labs(
+  x = "Observed GDD",
+  y = "Simulated data"
+)
+
+theme_set(bayesplot::theme_default(base_size = 18))
+theme_update(axis.text = element_text(size = 20))
+
+ggplot(data2, aes(x = gdd, y = sim2)) + 
+  geom_point(alpha = 0.1, color = "red") + 
+  xysim_labs + coord_cartesian(xlim=c(0, 1000), ylim=c(0,1000)) + geom_abline(intercept=0, slope=1)
+
+data3 <- data.frame(
+  gdd = fakedata_ws$gdd, 
+  wip = data2$sim, 
+  vague = data1$sim
+)
+ggplot(data3, aes(x=gdd, y=wip)) + 
+  geom_point(alpha = 0.1) + 
+  geom_point(
+    aes(y = vague), 
+    color = "red", 
+    alpha = 0.1
+  ) + 
+  xysim_labs + coord_cartesian(xlim=c(0, 1000), ylim=c(0,1000)) + geom_abline(intercept=0, slope=1)
+
+
+####################### NOW LET'S TRY TO ADDRESS QUESTION #2
+# Compare urban effect using weather station data and then hobo logger data
+# 2) GDDlo ~ urban + (urban|species) - do once with weather station data and once with hobo logger data
+
+### Okay, now let's make some fake data using help Rethinking, Gelman, OSPREE and Geoff
+#  1) Let's make the observations much higher than the actual data to build a good model.
+nsp = 20 # number of species
+ntot = 100 # numbers of obs per species. 
+nsite = 2
+
+sample_a <- list(int.env = rnorm(1000, 700, 200),
+                 site.env = rnorm(1000, 0, 50))
+
+#  2) Now, we will make varying intercepts
+env.samples <- sapply(sample_a, FUN = function(x){
+  sample(x, size = nsp * ntot, replace = TRUE)})
+
+#  4) We need to make a random intercept model for each species
+baseslope <- list(slope = mean(env.samples[,2]))
+baseslope.mat <- matrix(unlist(baseslope), ncol = length(baseslope), nrow = nsp * ntot, byrow = TRUE)
+## From Geoff's simulate-linear.R code in OSPREE: Which parameters are random?
+random.slope <- grep(pattern = paste("slope", collapse = "|"), x = names(baseslope))
+# Generate random intercepts (by species) 
+for(i in 1:length(random.slope)){
+  baseslope.mat[, i] <- sapply(1:nsp, FUN = function(X){
+    rep(rnorm(n = 1, mean = baseslope[[random.slope[i]]], sd = 50), ntot)})}
+
+#  5) Calculate response
+response <- sapply(1:nrow(env.samples), FUN = function(x){
+  rnorm(n = 1, mean = baseslope.mat[x, ], sd = 50)})
+#  6) Make a dataframe of fake data
+fakedata_ws <- cbind(data.frame(species = as.vector(sapply(1:nsp, FUN = function(x) rep(x, ntot))),
+                                gdd = response, urban = rbinom(1000, 1, 0.5)))
+
+#  7) Let's do a quick lmer model to test the fake data
+library(lme4)
+modtest <- lmer(gdd ~ urban + (urban|species), data=fakedata_ws) ## Quick look looks good!
+
+library(RColorBrewer)
+my.pal <- rep(brewer.pal(n = 10, name = "Paired"), 2)
+my.pch <- rep(15:16, each=10)
+plot(gdd ~ species, col=my.pal[species], pch=my.pch[species], data = fakedata_ws)
+abline(h=mean(fakedata_ws$gdd), lwd=3)
+
+plot(density(fakedata_ws$gdd))
+abline(v = mean(fakedata_ws$gdd), lwd = 2, col = "blue")
+
+## PRIOR PREDICTIVE CHECK time!!
+# Now I will follow the workflow from the Gabry et al., 2019 paper
+## Using vague priors
+nsims <- length(fakedata_ws$species)
+alpha <- rnorm(20, 600, 50)
+sigma <- runif(20, 0, 50)
+
+data1 <- data.frame(
+  gdd = fakedata_ws$gdd,
+  sim = alpha[fakedata_ws$species] +
+    rnorm(nsims, mean = 0, sd = sigma)
+)
+
+xysim_labs <- labs(
+  x = "Observed GDD",
+  y = "Simulated data"
+)
+
+theme_set(bayesplot::theme_default(base_size = 18))
+theme_update(axis.text = element_text(size = 20))
+
+ggplot(data1, aes(x = gdd, y = sim)) + 
+  geom_point(alpha = 0.1, color = "red") + 
+  xysim_labs + coord_cartesian(xlim=c(250, 1000), ylim=c(250,1000)) + geom_abline(intercept=0, slope=1)
+
+## Using weakly informative priors
+alpha2 <- rnorm(20, 0, 1)
+sigma2 <- runif(20, 0, 1)
+
+data2 <- data.frame(
+  gdd = fakedata_ws$gdd,
+  sim2 = alpha2[fakedata_ws$species] +
+    rnorm(nsims, mean = 0, sd = sigma)
+)
+
+xysim_labs <- labs(
+  x = "Observed GDD",
+  y = "Simulated data"
+)
+
+theme_set(bayesplot::theme_default(base_size = 18))
+theme_update(axis.text = element_text(size = 20))
+
+ggplot(data2, aes(x = gdd, y = sim2)) + 
+  geom_point(alpha = 0.1, color = "red") + 
+  xysim_labs + coord_cartesian(xlim=c(0, 1000), ylim=c(0,1000)) + geom_abline(intercept=0, slope=1)
+
+data3 <- data.frame(
+  gdd = fakedata_ws$gdd, 
+  wip = data2$sim, 
+  vague = data1$sim
+)
+ggplot(data3, aes(x=gdd, y=wip)) + 
+  geom_point(alpha = 0.1) + 
+  geom_point(
+    aes(y = vague), 
+    color = "red", 
+    alpha = 0.1
+  ) + 
+  xysim_labs + coord_cartesian(xlim=c(0, 1000), ylim=c(0,1000)) + geom_abline(intercept=0, slope=1)
 
