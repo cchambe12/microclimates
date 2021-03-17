c8db95f87a9a4fa276034c4e7f73ef348ad165f4
cchambe12 <cchamberlain@g.harvard.edu>
2021-03-17 17:11:26 -0400

    updates to outline and forecasts

--- analyses/forecasting.R
+++ analyses/forecasting.R
@@ -14,11 +14,13 @@ setwd("~/Documents/git/microclimates/analyses/")
 
 source("source/sims_warm_sourcedata.R")
 
-gdd.warm <- warmfunc(150, 350, 10, 5, 10)
+#### warmfunc(fstar.min, fstar.max, warmmax, meantemp, basetemp)
+
+gdd.warm <- warmfunc(100, 300, 10, 10, 10)
 gdd.warm <- gdd.warm[[1]]
 
 pdf("figures/forecasting_base10.pdf", width=6, height=5)
-ggplot(gdd.warm, aes(as.numeric(warming), gdd_accuracy, col=fstarspp, group=fstarspp)) +
+ggplot(gdd.warm, aes(as.numeric(warming), gdd_ratio, col=fstarspp, group=fstarspp)) +
   geom_line(stat='smooth', method = "lm", alpha=1, aes(col=fstarspp)) +
   #geom_point(aes(as.numeric(warming), gdd_accuracy, col=fstarspp, group=fstarspp)) +
   theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
@@ -27,10 +29,11 @@ ggplot(gdd.warm, aes(as.numeric(warming), gdd_accuracy, col=fstarspp, group=fsta
         plot.margin = margin(0.5, 0.5, 0.5, 1, "cm")) + 
   xlab("Amount of warming (°C)") + ylab("GDD accuracy\n(estimated/observed)") +
   scale_x_continuous(breaks = sort(c(seq(0, 10, by=1)))) +
-  scale_color_viridis_c("Species GDD Threshold")
+  scale_color_viridis_c("Species GDD Threshold") +
+  coord_cartesian(ylim=c(1, 1.3)) 
 dev.off()
 
-gdd.warm <- warmfunc(150, 350, 10, 5, 5)[[1]]
+gdd.warm <- warmfunc(100, 300, 10, 10, 5)[[1]]
 
 pdf("figures/forecasting_base5.pdf", width=6, height=5)
 ggplot(gdd.warm, aes(as.numeric(warming), gdd_ratio, col=fstarspp, group=fstarspp)) +
@@ -42,11 +45,12 @@ ggplot(gdd.warm, aes(as.numeric(warming), gdd_ratio, col=fstarspp, group=fstarsp
         plot.margin = margin(0.5, 0.5, 0.5, 1, "cm")) + 
   xlab("Amount of warming (°C)") + ylab("GDD accuracy\n(estimated/observed)") +
   scale_x_continuous(breaks = sort(c(seq(0, 10, by=1)))) +
-  scale_color_viridis_c("Species GDD Threshold")
+  scale_color_viridis_c("Species GDD Threshold") +
+  coord_cartesian(ylim=c(1, 1.3)) 
 dev.off()
 
 
-gdd.warm <- warmfunc(150, 350, 10, 5, 0)[[1]]
+gdd.warm <- warmfunc(100, 300, 10, 10, 0)[[1]]
 
 pdf("figures/forecasting_base0.pdf", width=6, height=5)
 ggplot(gdd.warm, aes(as.numeric(warming), gdd_ratio, col=fstarspp, group=fstarspp)) +
@@ -58,21 +62,23 @@ ggplot(gdd.warm, aes(as.numeric(warming), gdd_ratio, col=fstarspp, group=fstarsp
         plot.margin = margin(0.5, 0.5, 0.5, 1, "cm")) + 
   xlab("Amount of warming (°C)") + ylab("GDD accuracy\n(estimated/observed)") +
   scale_x_continuous(breaks = sort(c(seq(0, 10, by=1)))) +
-  scale_color_viridis_c("Species GDD Threshold")
+  scale_color_viridis_c("Species GDD Threshold") +
+  coord_cartesian(ylim=c(1, 1.3))
 dev.off()
 
-gdd.warm <- warmfunc(150, 350, 10, 5, -5)[[1]]
+gdd.warm <- warmfunc(100, 300, 10, 10, -5)[[1]]
 
 pdf("figures/forecasting_base-5.pdf", width=6, height=5)
 ggplot(gdd.warm, aes(as.numeric(warming), gdd_ratio, col=fstarspp, group=fstarspp)) +
   geom_line(stat='smooth', method = "lm", alpha=1, aes(col=fstarspp)) +
-  geom_point(aes(as.numeric(warming), gdd_accuracy, col=fstarspp, group=fstarspp)) +
+  #geom_point(aes(as.numeric(warming), gdd_accuracy, col=fstarspp, group=fstarspp)) +
   theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
         legend.text.align = 0,
         legend.key = element_rect(colour = "transparent", fill = "white"),
         plot.margin = margin(0.5, 0.5, 0.5, 1, "cm")) + 
   xlab("Amount of warming (°C)") + ylab("GDD accuracy\n(estimated/observed)") +
   scale_x_continuous(breaks = sort(c(seq(0, 10, by=1)))) +
-  scale_color_viridis_c("Species GDD Threshold")
+  scale_color_viridis_c("Species GDD Threshold") +
+  coord_cartesian(ylim=c(1, 1.3)) 
 dev.off()
 
