19bb88c1feee0007da88a469d99f3eab8cd51fab
cchambe12 <cchamberlain@g.harvard.edu>
2021-05-12 08:12:06 -0400

    working on func types

--- analyses/modoutput_functype.R
+++ analyses/modoutput_functype.R
@@ -75,6 +75,7 @@ diffinter_real <- ggplot(bbdiff, aes(x=meanshrub, y=meantree, col=as.factor(site
   #geom_text(aes(label=species), vjust=2) + 
   xlab("GDDs until budburst \n(shrubs)") + 
   ylab("GDDs until budburst \n(trees)") + 
+  geom_line(x=1, y=1, slope=1, linetype="dashed", color="black") +
   ggtitle("a)") +
   scale_color_manual(name=expression(Delta*" in GDDs"), values=cols,
                      labels=c("hfws"="Forest site: weather station \n(57.99)", "arbws"="Urban site: weather station \n(-95.45)",
